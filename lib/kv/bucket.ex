d6b4d68f8198743faf0d580dfa1082f357c5b715
Ola Sendecka <ola@hauru.eu>
2016-06-15 01:03:55 +0100

    Change KV.Bucket to match tutorial.

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -25,6 +25,8 @@ defmodule KV.Bucket do
     Returns the `value`.
   """
   def delete(bucket, key) do
-    Agent.get_and_update(bucket, &Map.pop(&1, key))
+    Agent.get_and_update(bucket, fn dict ->
+      Map.pop(dict, key)
+    end)
   end
 end
